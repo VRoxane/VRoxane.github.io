3b4ae8d4a3231cf0bc04e32bc43bad3839cbbb6f
Roxane 3SC <Roxane 3SC>
2016-04-04 12:08:51 +0200

    Je crois que j'ai tout fini

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage();
-	$(document).foundation();
+$(document).foundation();
+$(function() {
+    console.log( "ready!" );
+    $('#fullpage').fullpage({
+    	anchors:['nav_CV', 'nav_webdesign', 'nav_ux', 'nav_logo', 'nav_photo'],
+    	onLeave: function(index, nextIndex, direction){
+            var leavingSection = $(this);
+
+            //after leaving section 2
+            if(index == 1 && direction =='down'){
+                $('#nav').toggleClass('open');
+            }
+
+            else if(index == 2 && direction == 'up'){
+                
+                $('#nav').toggleClass('open');
+            }
+        }
+    });
 });
